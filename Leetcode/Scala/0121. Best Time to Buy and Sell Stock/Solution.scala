a8e68f0037b8bf93a3e63ceaeac9b37f92f8c84a
varushvarsha <varush.varsha777@gmail.com>
2024-08-14 19:43:43 +0530

    update

---
+++ Leetcode/Scala/0121. Best Time to Buy and Sell Stock/Solution.scala
@@ -0,0 +1,12 @@
+
+
+
+
+object Solution {
+    def maxProfit(prices: Array[Int]): Int = {
+        prices.foldLeft((0, prices(0))) { case ((res, minSoFar), price) =>
+            (Math.max(res, price - minSoFar), Math.min(price, minSoFar))
+        }._1
+    }
+}
+
