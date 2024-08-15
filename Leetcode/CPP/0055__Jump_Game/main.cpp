6cf135b4a7b995545957805b945253e038c902cc
varushvarsha <varush.varsha777@gmail.com>
2024-08-16 01:57:57 +0530

    added

---
+++ Leetcode/CPP/0055__Jump_Game/main.cpp
@@ -0,0 +1,31 @@
+
+
+
+
+#include <bits/stdc++.h>
+
+using std::vector;
+
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int const n = nums.size();
+        int possible = 0;
+        for(int i = 0; i < n; ++i) {
+            if(possible  <   i) { return false; }
+            if(possible >= n-1) { return true;  }
+            possible = std::max(possible, i + nums[i]);
+        }
+        return true;
+    }
+};
+
+
+int main() {
+    Solution sol;
+    std::vector<int> nums = {2,3,1,1,4};
+    std::cout.setf(std::ios::boolalpha);
+    std::cout << sol.canJump(nums) << '\n';
+    
+    return 0;
+}
