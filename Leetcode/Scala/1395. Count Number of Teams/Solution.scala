ff54c827463b72b5676abcf1fd8fff71213e17f0
varushvarsha <varush.varsha777@gmail.com>
2024-08-16 05:37:05 +0530

    added

--- Leetcode/Scala/1395. Count Number of Teams/Solution.scala
+++ Leetcode/Scala/1395. Count Number of Teams/Solution.scala
@@ -5,7 +5,7 @@
 object Solution {
     def numTeams(rating: Array[Int]): Int = {
        rating.indices.foldLeft(0) { case (res, i) =>
-            val lower = rating.take(i).count(_ < rating(i))
+            val lower  = rating.take(i).count(_ < rating(i))
             val higher = rating.drop(i + 1).count(_ > rating(i))
             res + lower * higher + (i - lower) * (rating.length - i - 1 - higher)
         }
