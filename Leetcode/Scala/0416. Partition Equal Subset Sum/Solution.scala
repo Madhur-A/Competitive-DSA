047ffd3bf211d900f03862fd365ddcfd0df4e039
varushvarsha <varush.varsha777@gmail.com>
2024-08-18 09:51:02 +0530

    added

---
+++ Leetcode/Scala/0416. Partition Equal Subset Sum/Solution.scala
@@ -0,0 +1,24 @@
+
+
+
+
+
+
+object Solution {
+    def canPartition(nums: Array[Int]): Boolean = {
+        val (total, target) = (nums.sum, nums.sum / 2)
+        total match {
+            case x if (total & 1) == 1 =>  false 
+            case _                     =>  {
+                val dp = Array.fill(target + 1)(false)
+                dp.update(0, true)
+                nums.foldLeft(dp) { case (dp, num) => 
+                    (target to num by -1).foreach { subTarget =>
+                        dp.update(subTarget, dp(subTarget) | dp(subTarget - num))
+                    }; dp
+                }.last
+            }
+        }
+    }
+}
+
