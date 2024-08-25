a15c3a365de282e672019c07ea78b8a92a5ad8c9
varushvarsha <varush.varsha777@gmail.com>
2024-08-25 08:52:14 +0530

    added

--- Leetcode/Java/0564. Find the Closest Palindrome/Solution.java
+++ Leetcode/Java/0564. Find the Closest Palindrome/Solution.java
@@ -6,10 +6,6 @@ import java.util.HashSet;
 import java.util.PriorityQueue;
 import java.util.Set;
 
-
-
-
-
 public class Solution {
 	public class Pair<T extends Comparable<T>> implements Comparable<Pair<T>> {
 		T first, second;
