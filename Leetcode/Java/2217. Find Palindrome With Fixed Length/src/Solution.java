410c1bc0a592764041d4ed429158c2a6d9794491
varushvarsha <varush.varsha777@gmail.com>
2024-08-25 12:58:21 +0530

    added

--- Leetcode/Java/2217. Find Palindrome With Fixed Length/src/Solution.java
+++ Leetcode/Java/2217. Find Palindrome With Fixed Length/src/Solution.java
@@ -2,12 +2,7 @@
 
 
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class Solution {
-	public Map<Long, Long> past = new HashMap<>();
-
 	public long power(int count, long res) {
 		return (count == 0) ? res : power(count - 1, 10L * res);
 	}
@@ -19,19 +14,16 @@ public class Solution {
 	}
 
 	public long getKthPalindrome(int len, long k) {
-		if(! past.containsKey(k)) {
-			int mid = (len >> 1) - (((len & 1) == 1) ? 0 : 1);
-			final long left = power(mid, 1L), right = power(mid + 1, 1L);
-			final long base = ((len & 1) == 1) ? left : right;
-			final long sffx = ((len & 1) == 1) ? 10L  : 1L;
-			long res = -1L;
-			if(k <= right - left) {
-				long num = left + k - 1;
-				res = num * base + reverseNumber(num / sffx);
-			}
-			past.put(k, res);
-		}
-		return past.get(k);
+        int mid = (len >> 1) - (((len & 1) == 1) ? 0 : 1);
+        final long left = power(mid, 1L), right = power(mid + 1, 1L);
+        final long base = ((len & 1) == 1) ? left : right;
+        final long sffx = ((len & 1) == 1) ? 10L  : 1L;
+        long res = -1L;
+        if(k <= right - left) {
+            long num = left + k - 1;
+            res = num * base + reverseNumber(num / sffx);
+        }
+        return res;
 	}
 
     public long[] kthPalindrome(int[] queries, int intLength) {
@@ -44,3 +36,5 @@ public class Solution {
     	return buffer;
     }
 }
+
+
