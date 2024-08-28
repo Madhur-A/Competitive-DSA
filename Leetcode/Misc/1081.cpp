84a9ac773d2ffcd677703d27773f20d7663e4f25
varushvarsha <varush.varsha777@gmail.com>
2024-08-28 11:02:31 +0530

    added

--- Leetcode/Misc/1081.cpp
+++ Leetcode/Misc/1081.cpp
@@ -2,25 +2,32 @@
 
 
 
+int _ = [](){ std::ios_base::sync_with_stdio(false); std::cin.tie(nullptr); return 0; }();
+
 class Solution {
 public:
-    std::unordered_set<int> seen;
     std::string ans;
 public:
     string smallestSubsequence(string s) {
         int const n = s.length();
-        std::vector<int> g(26);
-        for(int i = 0; i < n; ++i) { g[s[i]-'a'] = i; }
-        for(int i = 0; i < n; ++i) {
+        std::vector<int> last_indices(26), seen(26);
+
+		for(int i = 0; i < n; ++i) {
+			last_indices[s[i] - 'a'] = i;
+		}
+
+		for(int i = 0; i < n; ++i) {
             char const c = s[i];
-            if(not seen.count(c)) { 
-				while(not ans.empty() and c < ans.back() and i < g[ans.back()-'a']) {
-					seen.erase(ans.back()); ans.pop_back();
+            if(not seen[c - 'a']) {
+				while(not ans.empty() and c < ans.back() and i < last_indices[ans.back()-'a']) {
+					seen[ans.back() - 'a'] = 0;
+					ans.pop_back();
 				}
 				ans.push_back(c);
-				seen.insert(c);
+				seen[c - 'a'] = 1;
 			}
         }
         return ans;
     }
 };
+
--- Leetcode/Misc/README.md
+++ Leetcode/Misc/README.md
@@ -1,5 +1,5 @@
 # Misc
 
-Screenshots and some common programs. Screenshots and programs in this directory may depcit some unusual, non-optimal solutions or at times incorrect/incomplete solutions. 
+Screenshots and some general programs. Screenshots and programs in this directory may depcit some unusual, non-optimal solutions or at times incorrect/incomplete solutions. 
 
 
