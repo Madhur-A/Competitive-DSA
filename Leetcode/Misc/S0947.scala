50cdf5341a46945b5dbdea9fa86eab6c1cefe50e
varushvarsha <varush.varsha777@gmail.com>
2024-08-29 18:42:37 +0530

    added

--- Leetcode/Misc/S0947.scala
+++ Leetcode/Misc/S0947.scala
@@ -2,8 +2,6 @@
 
 
 
-import scala.collection.mutable.{Set, ArrayBuffer}
-
 class MergeFind private (val roots: Array[Int], val ranks: Array[Int]) {
     def this(numberOfElements: Int) = {
         this(Array.tabulate(numberOfElements + 10)(identity), new Array[Int](numberOfElements + 10))
@@ -30,7 +28,7 @@ class MergeFind private (val roots: Array[Int], val ranks: Array[Int]) {
     }
 
     def uniqueRoots(edges: Array[Array[Int]]): Int = {
-        edges.length -
+        edges.length - 
         edges.foldLeft(scala.collection.mutable.Set[Int]()) { case (seen, edge) =>
             seen.addOne(find(edge.head)); seen
         }.size
@@ -39,8 +37,9 @@ class MergeFind private (val roots: Array[Int], val ranks: Array[Int]) {
 
 object MergeFind {
     def apply(edges: Array[Array[Int]]): Int = {
-        val mergeFind = new MergeFind(edges.flatten.max + edges.flatten.max + 1)
-        edges.foreach { edge => mergeFind.merge(edge.head, edge.last + edges.flatten.max + 1) }
+        val maxVertex = edges.flatten.max
+        val mergeFind = new MergeFind(maxVertex + maxVertex + 1)
+        edges.foreach { edge => mergeFind.merge(edge.head, edge.last + maxVertex + 1) }
         mergeFind.uniqueRoots(edges)
     }
 }
@@ -48,5 +47,7 @@ object MergeFind {
 object Solution {
     def removeStones(stones: Array[Array[Int]]): Int = {
         MergeFind(stones)
-    }
+    }    
+
+    def response: String = removeStones(Array(Array(0,1),Array(1,0))).toString
 }
