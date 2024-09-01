395574bcd84646e372a8971f74125c09a8170428
varushvarsha <varush.varsha777@gmail.com>
2024-09-01 19:22:12 +0530

    added

---
+++ Leetcode/Misc/S0300.scala
@@ -0,0 +1,17 @@
+
+
+
+
+object Solution {
+    def lengthOfLIS(nums: Array[Int]): Int = {
+        nums.indices.drop(1).foldLeft(Array.fill(nums.length)(1)) { case (dp, index) =>
+            var innerMax = 0
+            dp.take(index).indices.foreach { j =>
+                if (nums(j) < nums(index)) {
+                    innerMax = Math.max(innerMax, Math.max(dp(index), dp(j) + 1))
+                }
+            }
+            dp.update(index, Math.max(dp(index), innerMax)); dp
+        }.max        
+    }
+}
