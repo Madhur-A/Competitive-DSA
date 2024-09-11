8b28ed3400fd521dbd1e98a9de51e1287e073480
varushvarsha <varush.varsha777@gmail.com>
2024-09-11 15:02:24 +0530

    added

--- Leetcode/Erlang/2708. Maximum Strength of a Group/sol/sol.erl
+++ Leetcode/Erlang/2708. Maximum Strength of a Group/sol/sol.erl
@@ -1,12 +1,15 @@
-%% -module(sol).
+-module(sol).
 -compile(export_all). % testing phase directive
 
 -spec max_strength(Nums :: [integer()]) -> integer().
 
-rx(Nums, Curr) when Nums == [] -> Curr;
-rx([Front|Rest], Curr) when Front == 0 -> rx(Rest, Curr);
+rx([], Curr) -> Curr;
+rx([0|Rest], Curr) -> rx(Rest, Curr);
 rx([Front|Rest], Curr) -> max(rx(Rest, Curr), max(rx(Rest, Curr * Front), rx(Rest, Front))).
 
-max_strength([Front|Rest]) when Rest == [] -> Front;
+
+max_strength([Front|[]]) -> Front;
 max_strength(Nums) -> rx(Nums, 0).
 
+clear() ->
+	io:format("\ec").
