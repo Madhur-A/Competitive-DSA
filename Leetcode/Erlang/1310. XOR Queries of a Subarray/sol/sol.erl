487796d7bba62a29cff3792985212f35a0b992b2
varushvarsha <varush.varsha777@gmail.com>
2024-09-13 20:24:39 +0530

    added

--- Leetcode/Erlang/1310. XOR Queries of a Subarray/sol/sol.erl
+++ Leetcode/Erlang/1310. XOR Queries of a Subarray/sol/sol.erl
@@ -1,11 +1,12 @@
 %% -module(sol).
 -compile(export_all). % testng phase directive
 
--spec xor_queries(Arr :: [integer()], Queries :: [[integer()]]) -> [integer()].
+
+%% -spec xor_queries(Arr :: [integer()], Queries :: [[integer()]]) -> [integer()].
 
 xor_queries(Nums, Queries) -> 
-	P = lists:foldl(fun(E, R) -> R ++ [lists:last(R) bxor E] end, [0], Nums),
-	lists:map(fun(Q) -> lists:nth(hd(tl(Q)) + 2, P) bxor lists:nth(hd(Q) + 1, P) end, Queries).
+    P = lists:reverse(lists:foldl(fun(E, R) -> [hd(R) bxor E] ++ R end, [0], Nums)),
+    lists:map(fun([A, B]) -> lists:nth(B + 2, P) bxor lists:nth(A + 1, P) end, Queries).
 
 clear() ->
-	io:format("\ec").
+    io:format("\ec").
