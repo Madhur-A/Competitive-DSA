ec1ff78f3ed63e85cbb8a64f6f497ceed90c3e7d
varushvarsha <varush.varsha777@gmail.com>
2024-09-13 16:35:08 +0530

    added

---
+++ Leetcode/Erlang/0102. Binary Tree Level Order Traversal/sol/sol_improved.erl
@@ -0,0 +1,26 @@
+ %% -module(sol_improved).
+-compile(export_all). % testing phase directive
+
+%% Definition for a binary tree node.
+%%
+%% -record(tree_node, {val = 0 :: integer(),
+%%                     left = null  :: 'null' | #tree_node{},
+%%                     right = null :: 'null' | #tree_node{}}).
+
+-spec level_order(Root :: #tree_node{} | null) -> [[integer()]].
+
+get_nodes(null,  null) -> [];
+get_nodes(null, Right) -> [Right];
+get_nodes(Left,  null) -> [Left];
+get_nodes(Left, Right) -> [Left, Right].
+
+lrx(   [], Res) -> Res;
+lrx(Roots, Res) ->
+	lrx(lists:flatten([get_nodes(Root#tree_node.left, Root#tree_node.right) || Root <- Roots, Root =/= null]),
+		Res ++ [[Root#tree_node.val || Root <- Roots, Root =/= null]]).
+
+level_order(null) -> [];
+level_order(Root) -> lrx([Root], []).
+
+clear() ->
+	io:format("\ec").
