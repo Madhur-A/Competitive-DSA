c8f665382c19033379591860ed20b4e0893c6db5
varushvarsha <varush.varsha777@gmail.com>
2024-09-14 14:58:54 +0530

    added

--- Leetcode/Erlang/2419. Longest Subarray With Maximum Bitwise AND/sol/sol.erl
+++ Leetcode/Erlang/2419. Longest Subarray With Maximum Bitwise AND/sol/sol.erl
@@ -4,17 +4,17 @@
 
 -spec longest_subarray(Nums :: [integer()]) -> integer().
 
-longest([],      _,      _,     _, Ans) -> Ans;
-longest(Nums, MaxE,      _,     _,   _) when hd(Nums) > MaxE -> 
-    longest(tl(Nums), hd(Nums), true, 1, 1);
-longest(Nums, MaxE, Streak, Found, Ans) when hd(Nums) =:= MaxE andalso Streak =:= true -> 
-    longest(tl(Nums), MaxE, Streak, Found + 1, max(Ans, Found + 1));
-longest(Nums, MaxE,      _, Found, Ans) ->
-    longest(tl(Nums), MaxE, false, Found, Ans).
+longest(        [],    _,      _, _, R) -> R;
+longest([Num|Rest], MaxE,      _, _, R) when Num < MaxE -> longest(Rest, MaxE, false, 0, R);    
+longest([Num|Rest], MaxE,      _, _, _) when Num > MaxE -> longest(Rest, Num, true, 1, 1);
+longest([Num|Rest], MaxE, Streak, F, R) when Num =:= MaxE ->
+	case Streak =:= true of
+		true -> longest(Rest, MaxE, Streak, F + 1, max(R, F + 1));
+		_    -> longest(Rest, MaxE, true, 1, R)
+	end.
 
-longest_subarray(Nums) -> 
-    longest(Nums, -1, false, 0, 0).
+longest_subarray(Nums) ->
+    longest(Nums, 0, false, 0, 0).
 
 clear() ->
 	io:format("\ec").
-
