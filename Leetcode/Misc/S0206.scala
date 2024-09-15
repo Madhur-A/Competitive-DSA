59af849fea20b33719d088747d010ef60c4d5224
varushvarsha <varush.varsha777@gmail.com>
2024-09-16 01:06:14 +0530

    added

--- Leetcode/Misc/S0206.scala
+++ Leetcode/Misc/S0206.scala
@@ -3,14 +3,10 @@
 
 
 object Solution {
-    def reverseList(root: ListNode): ListNode = {
-        if (root != null) {
-            if (root.next == null) { root } else {
-                val returnOfTheRoot = reverseList(root.next)
-                root.next.next = root
-                root.next = null
-                returnOfTheRoot
-            }
-        } else { null }
+    def reverseList(head: ListNode, rest: ListNode = null): ListNode = {
+        if (head == null) { rest } else {
+            reverseList(head.next, ListNode(head.x, rest))
+        }
     }
 }
+
