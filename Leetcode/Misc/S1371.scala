e4cb14f06b7fc60bae3aea103e4164d91f52c10b
varushvarsha <varush.varsha777@gmail.com>
2024-09-15 16:06:20 +0530

    added

---
+++ Leetcode/Misc/S1371.scala
@@ -0,0 +1,21 @@
+
+
+
+
+object Solution {
+    def findTheLongestSubstring(s: String): Int = {
+        val vowels = "aeiou"
+        val positions = Array.fill(32)(Int.MaxValue)
+        positions.update(0, -1)
+
+        s.zipWithIndex.foldLeft((0, 0)) { case ((res, state), (char, index)) =>
+            val newState = vowels.indexOf(char) match {
+                case -1 => state
+                case  j => state ^ (1 << j)
+            }
+            val newRes = math.max(res, index - positions(newState))
+            positions(newState) = math.min(positions(newState), index)
+            (newRes, newState)
+        }.head
+    }
+}
--- Leetcode/Scala/0129. Sum Root to Leaf Numbers/Solution.scala
+++ Leetcode/Scala/0129. Sum Root to Leaf Numbers/Solution.scala
@@ -3,10 +3,10 @@
 
 
 object Solution {
-    def rx(node: TreeNode, curr: List[Int] = List[Int](), res: Int = 0): Int = {
+    def rx(node: TreeNode, curr: List[Int] = List[Int]()): Int = {
         if (node == null) { 0 } else {
-            if (node.left == null && node.right == null) { res + (curr :+ node.value).mkString.toInt } else {
-                rx(node.left, curr :+ node.value, res) + rx(node.right, curr :+ node.value, res)
+            if (node.left == null && node.right == null) { (curr :+ node.value).mkString.toInt } else {
+                rx(node.left, curr :+ node.value) + rx(node.right, curr :+ node.value)
             }
         }
     }
