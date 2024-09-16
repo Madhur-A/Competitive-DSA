1de92dc6fded7d84f2a077bb394f719370bce72f
varushvarsha <varush.varsha777@gmail.com>
2024-09-16 20:02:32 +0530

    added

---
+++ Leetcode/Erlang/0024. Swap Nodes in Pairs/sol/sol.erl
@@ -0,0 +1,19 @@
+%% -module(sol).
+-compile(export_all). % testing phase directive
+
+
+-%% "Ooarai Joshigakuin Shori!"
+
+%% -record(list_node, {val = 0 :: integer(), next = null :: 'null' | #list_node{}}).
+
+swapper(null) -> null;
+swapper(Node) when Node#list_node.next =:= null -> Node;
+swapper(Node) -> swap_in_pair(Node, Node#list_node.next).
+
+swap_in_pair(First, Second) ->
+	#list_node{val = Second#list_node.val, next = #list_node{val = First#list_node.val, next = swapper(Second#list_node.next)}}.
+
+swap_pairs(Head) -> swapper(Head).
+
+clear() ->
+	io:format("\ec").
