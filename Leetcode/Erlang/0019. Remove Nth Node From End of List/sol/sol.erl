bedc96a730163c77580b9bcf3a8e12cdb637d9bf
varushvarsha <varush.varsha777@gmail.com>
2024-09-17 02:34:05 +0530

    added

---
+++ Leetcode/Erlang/0019. Remove Nth Node From End of List/sol/sol.erl
@@ -0,0 +1,33 @@
+%% -module(sol).
+-compile(export_all). % testing phase directive
+-author("Miho Nishizumi"). % "Ooarai Joshigakuin Shori!"
+
+-spec remove_nth_from_end(Head :: #list_node{} | null, N :: integer()) -> #list_node{} | null.
+
+%% Definition for singly-linked list.
+%%
+%% -record(list_node, {val = 0 :: integer(),
+%%                     next = null :: 'null' | #list_node{}}).
+
+hook(null) -> null;
+hook(Sinker) ->
+	case Sinker#list_node.next =/= null of
+		true -> #list_node{val = Sinker#list_node.val, next = Sinker#list_node.next#list_node.next};
+		false -> null
+	end.
+
+slow_tracker(SlowSinker,       null) -> hook(SlowSinker);
+slow_tracker(SlowSinker, FastSinker) ->
+	#list_node{val = SlowSinker#list_node.val, next = slow_tracker(SlowSinker#list_node.next, FastSinker#list_node.next)}.
+
+fast_tracker(TrueSinker, Sinker, FCount, N) when FCount < N ->
+	fast_tracker(TrueSinker, Sinker#list_node.next, FCount + 1, N);
+fast_tracker(TrueSinker, Sinker, _, _) ->
+	case Sinker =/= null of
+		true -> slow_tracker(TrueSinker, Sinker#list_node.next);
+		false  -> TrueSinker#list_node.next
+	end.
+
+tracker(Sinker, N) -> fast_tracker(Sinker, Sinker, 0, N).
+
+remove_nth_from_end(Head, N) -> tracker(Head, N).
