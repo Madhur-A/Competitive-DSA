6a0888076af9e924ecb1aad52b3e618c7115e95d
varushvarsha <varush.varsha777@gmail.com>
2024-09-16 09:21:05 +0530

    added

---
+++ Leetcode/Erlang/0539. Minimum Time Difference/sol/sol.erl
@@ -0,0 +1,22 @@
+%% -module(sol).
+-compile(export_all). % testing phase directive
+
+parse(InputString) ->
+	[First, Second] = string:split(InputString, ":"),
+	{{Hours, _}, {Minutes, _}} = {string:to_integer(First), string:to_integer(Second)},
+	Hours * 60 + Minutes.
+
+get_min([First, Second], Res) -> min(Second - First, Res);
+get_min([First, Second|Rest], Res) ->
+	get_min([Second|Rest], min((Second - First), Res)).
+
+minimum_difference(Points) ->
+	P = lists:sort(lists:map(fun(I) -> parse(I) end, Points)),
+	get_min(P ++ [hd(P) + 1440], 1 bsl 12).
+
+find_min_difference(TimePoints) when length(TimePoints) > 1440 -> 0;
+find_min_difference(TimePoints) -> 
+	minimum_difference(lists:map(fun(E) -> binary_to_list(E) end, TimePoints)).
+
+clear() ->
+	io:format("\ec").
