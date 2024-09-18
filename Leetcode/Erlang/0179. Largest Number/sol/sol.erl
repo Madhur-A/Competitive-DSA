2fa864c045755e507036b9ec42a52ffa995e8391
varushvarsha <varush.varsha777@gmail.com>
2024-09-18 20:40:30 +0530

    added

--- Leetcode/Erlang/0179. Largest Number/sol/sol.erl
+++ Leetcode/Erlang/0179. Largest Number/sol/sol.erl
@@ -1,4 +1,4 @@
-%% -module(sol). % "Ooarai Joshigakuin Shori!"
+-module(sol). % "Ooarai Joshigakuin Shori!"
 -compile(export_all). % testing phase directive
 
 
@@ -8,8 +8,10 @@ as_string([]) -> "";
 as_string([First|Rest]) ->
 	integer_to_list(First) ++ as_string(Rest).
 
+to_string(A) -> integer_to_list(A).	
+
 get_largest(Nums) ->
-	Res = as_string(lists:sort(fun(A, B) -> integer_to_list(A + B) < integer_to_list(B + A) end, Nums)),
+	Res = as_string(lists:sort(fun(A, B) -> to_string(A) ++ to_string(B) > to_string(B) ++ to_string(A) end, Nums)),
 	case hd(Res) =:= 48 of
 		true  -> unicode:characters_to_binary("0");
 		false -> unicode:characters_to_binary(Res)
