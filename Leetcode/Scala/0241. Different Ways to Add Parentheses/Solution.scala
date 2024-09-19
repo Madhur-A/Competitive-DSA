97e3027264f62bb16ea201adf89e69686f1d23f0
varushvarsha <varush.varsha777@gmail.com>
2024-09-19 17:14:50 +0530

    added

--- Leetcode/Scala/0241. Different Ways to Add Parentheses/Solution.scala
+++ Leetcode/Scala/0241. Different Ways to Add Parentheses/Solution.scala
@@ -2,6 +2,8 @@
 
 
 
+// with "local mutation" combined with imperative "if-else" construct
+// though still largely functional
 object Solution {
     def diffWaysToCompute(expression: String): List[Int] = {
         if (!expression.forall(Character.isDigit)) {
@@ -23,6 +25,32 @@ object Solution {
                     }.head
                 } else { res }
             }
+        } else { List(expression.toInt) }
+    }
+}
+
+
+// strictly functional version
+object Solution {
+    def diffWaysToCompute(expression: String): List[Int] = {
+        if (!expression.forall(Character.isDigit)) {
+            expression.zipWithIndex.flatMap { case (char, index) =>
+                if ("+-*".contains(char)) {
+                    val (left, right) = expression.splitAt(index)
+                    val lEval = diffWaysToCompute(left)
+                    val rEval = diffWaysToCompute(right.tail)
+                    for {
+                        lElem <- lEval
+                        rElem <- rEval
+                    } yield {
+                        char match {
+                            case '-' => lElem - rElem
+                            case '*' => lElem * rElem
+                            case '+' => lElem + rElem
+                        }
+                    }
+                } else { Nil }
+            }.toList
         } else { List(expression.toInt) }
     }
 }
