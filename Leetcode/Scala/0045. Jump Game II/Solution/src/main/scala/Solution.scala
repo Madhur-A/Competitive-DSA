08afd82405ce237e8021172cdaff97411bef6de4
varushvarsha <varush.varsha777@gmail.com>
2024-09-19 20:27:53 +0530

    added

--- Leetcode/Scala/0045. Jump Game II/Solution/src/main/scala/Solution.scala
+++ Leetcode/Scala/0045. Jump Game II/Solution/src/main/scala/Solution.scala
@@ -6,7 +6,7 @@ object Solution {
     def jump(nums: Array[Int]): Int = {
         nums.indices.dropRight(1).foldLeft((0, 0, 0, 0)) { case ((res, maxSoFar, maxIndex, index), i) =>
             val newMaxSoFar = Math.max(maxSoFar, i + nums(i))
-            if (i == index) then {
+            if (i == index) {
                 (res + 1, newMaxSoFar, newMaxSoFar, newMaxSoFar)
             } else {
                 (res, newMaxSoFar, maxIndex, index)
