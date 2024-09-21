dddb0ddd76384fcdf74c823d46d6d75f4851966d
varushvarsha <varush.varsha777@gmail.com>
2024-09-21 16:16:14 +0530

    added

---
+++ Leetcode/C/0043. Multiply Strings/main.c
@@ -0,0 +1,39 @@
+
+
+
+
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <stdlib.h>
+#include <limits.h>
+
+int pos[3000];
+char res[3000];
+
+char* multiply(char* num1, char* num2) {
+    int len1 = strlen(num1), len2 = strlen(num2);
+    memset(pos, 0, sizeof(pos));
+
+    for(int i = len1 - 1; 0 <= i; --i) {
+        for(int j = len2 - 1; 0 <= j; --j) {
+            int mul = (num1[i] - '0') * (num2[j] - '0');
+            int p1 = i + j, p2 = i + j + 1;
+            int sum = mul + pos[p2];
+
+            pos[p1] += sum / 10;
+            pos[p2]  = sum % 10;
+        }
+    }
+
+    int start = 0;
+    while(start < len1 + len2 && pos[start] == 0) { ++start; }
+
+    int x = 0;
+    for(int i = start; i <= len1 + len2 - 1; ++i) {
+        res[x++] = pos[i] + '0';
+    }
+    res[x] = '\0';
+
+    return res;
+}
