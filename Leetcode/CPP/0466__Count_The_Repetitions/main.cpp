bb20098a6a728ed8938ec262da4af671be6da02b
varushvarsha <varush.varsha777@gmail.com>
2024-09-23 14:03:09 +0530

    added

--- Leetcode/CPP/0466__Count_The_Repetitions/main.cpp
+++ Leetcode/CPP/0466__Count_The_Repetitions/main.cpp
@@ -8,25 +8,25 @@ using std::string;
 
 class Solution {
 public:
-    std::unordered_map<int, std::pair<int, int>> dp;
+    std::unordered_map<int, long> dp;
 public:
     int getMaxRepetitions(string s1, int n1, string s2, int n2) {
-        int m = s2.size();
-        for(int i = 0, reps = 0, j = 0; i < m; ++i) {
+        if(s1 == s2) { return n1 >= n2; }
+        int const m = s2.size(), c = std::max(n1 * s1.size(), n2 * s2.size()) + 1;
+        for(int i = 0, reps = 0, j = 0; i < m; ++i, reps = 0) {
             j = i;
             for(char const &c: s1) {
-                if(c == s2[j]) { j += 1; }
-                if(j == m) { reps += 1; j = 0; }
+				j += (c == s2[j]);
+				reps += (j == m);
+				j  = (j == m) ? 0 : j;
             }
-            dp[i] = std::make_pair(reps, j);
-            reps = 0;
+            dp[i] = reps * 1L * c + j;
         }
 
         int total = 0, next_index = 0;
         for(int i = 0; i < n1; ++i) {
-            std::pair<int, int> const e = dp[next_index];
-            next_index = e.second;
-            total += e.first;
+			total += dp[next_index] / c;
+            next_index = dp[next_index] % c;
         }
 
         return total / n2;
