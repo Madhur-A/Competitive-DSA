df09edbba322e99568d1b7f54a8288f3cb371c78
varushvarsha <varush.varsha777@gmail.com>
2024-09-23 22:11:07 +0530

    added

--- Leetcode/Erlang/1019. Next Greater Node In Linked List/sol/sol.erl
+++ Leetcode/Erlang/1019. Next Greater Node In Linked List/sol/sol.erl
@@ -9,14 +9,28 @@
 
 -spec next_larger_nodes(Head :: #list_node{} | null) -> [integer()].
 
-get_next(null,    _) -> [0];
-get_next(Head, Curr) ->
-		case Head#list_node.val > Curr of
-			true -> [Head#list_node.val];
-			false -> get_next(Head#list_node.next, Curr)
-		end.
+get_values(null, Res) -> Res;
+get_values(Head, Res) ->
+	get_values(Head#list_node.next, [Head#list_node.val] ++ Res).
 
-validate(null) -> [];
-validate(Head) -> get_next(Head, Head#list_node.val) ++ validate(Head#list_node.next).
+curtail_r([],     _) -> [];
+curtail_r([First|Rest],  Value) ->
+	case First =< Value of
+		true  -> curtail_r(Rest, Value);
+		false -> [First|Rest]
+	end.
 
-next_larger_nodes(Head) -> validate(Head).
+monotonic_fetch([], _, Ans) -> Ans;
+monotonic_fetch(V,  R, Ans) ->
+	[Top|Rest] = V,
+	NewR = curtail_r(R, Top),
+	case NewR =:= [] of
+		true  -> monotonic_fetch(Rest, [Top] ++ R, [0] ++ Ans);
+		false -> monotonic_fetch(Rest, [Top] ++ R, [hd(NewR)] ++ Ans)
+	end.
+
+next_greater(Node) ->
+	Values = get_values(Node, []),
+	monotonic_fetch(Values, [], []).
+
+next_larger_nodes(Head) -> next_greater(Head).
