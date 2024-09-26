d05f5faa5faaba01742d0448faf6e689b1e19359
varushvarsha <varush.varsha777@gmail.com>
2024-09-26 22:27:55 +0530

    added

---
+++ Leetcode/Erlang/0935. Knight Dialer/sol/sol.erl
@@ -0,0 +1,18 @@
+%% -module(sol).
+-compile(export_all). % testing phase directive
+% "Ooarai Joshigakuin Shori!"
+
+-spec knight_dialer(N :: integer()) -> integer().
+
+calculate(Index, N, Have) when Index =< N ->
+    A = 6 * array:get(Index - 2, Have),
+    B = 4 * array:get(Index - 4, Have),
+    calculate(Index + 1, N, array:set(Index, A - B, Have));
+calculate(_,     N, Have) -> array:get(N, Have).
+
+knight_dialer(N) ->
+    Have = array:from_list([0,10,20,46,104,240]),
+    case N < 6 of
+        true  -> array:get(N, Have);
+        false -> calculate(6, N, Have) rem 1000000007
+    end.
