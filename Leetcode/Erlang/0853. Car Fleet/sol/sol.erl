0caadc70cf627365bf226cf6a1a1628c41a0c755
varushvarsha <varush.varsha777@gmail.com>
2024-10-12 05:35:38 +0530

    added

--- Leetcode/Erlang/0853. Car Fleet/sol/sol.erl
+++ Leetcode/Erlang/0853. Car Fleet/sol/sol.erl
@@ -5,15 +5,12 @@
 -spec car_fleet(Target :: integer(), Position :: [integer()], Speed :: [integer()]) -> integer().
 
 rx(Positions, Speeds, Target) ->
-    	CurrentTarget = Target * 1.0,
-        {Ans, _ } =
-            lists:foldl(fun({Distance, Speed}, {Res, PrevTime}) ->
-                                CurrentTime = (CurrentTarget - Distance) / Speed,
-                                case CurrentTime > PrevTime of
-                                    true  -> {Res + 1, CurrentTime};
-                                    false -> {Res, PrevTime}
-                                end
-                            end, {0, 0.0}, lists:reverse(lists:sort(lists:zip(Positions, Speeds)))),
-        Ans.
+	element(1, lists:foldl(fun({Distance, Speed}, {Res, PrevTime}) ->
+								   CurrentTime = (Target - Distance) / Speed,
+								   case CurrentTime > PrevTime of
+									   true  -> {Res + 1, CurrentTime};
+									   false -> {Res, PrevTime}
+								   end
+						   end, {0, 0.0}, lists:reverse(lists:sort(lists:zip(Positions, Speeds))))).
 
 car_fleet(Target, Position, Speed) -> rx(Position, Speed, Target).
