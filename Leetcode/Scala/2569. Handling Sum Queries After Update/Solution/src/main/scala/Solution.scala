0a3c2c6b93fce94abec08225c15023de92ed3141
varushvarsha <varush.varsha777@gmail.com>
2024-10-14 15:02:58 +0530

    added

--- Leetcode/Scala/2569. Handling Sum Queries After Update/Solution/src/main/scala/Solution.scala
+++ Leetcode/Scala/2569. Handling Sum Queries After Update/Solution/src/main/scala/Solution.scala
@@ -3,75 +3,62 @@
 
 
 class SegmentTree(nums: Array[Int]) {
-    val n = nums.length
-    val t = Array.fill(4 * n)(0)
-    val l = Array.fill(4 * n)(0)
+    val     n  = nums.length
+    val (l, t) = (Array.fill(4 * n)(0), Array.fill(4 * n)(0))
 
     build(0, 0, n - 1)
 
-    def coupling(node: Int): (Int, Int) = (2 * node + 1, 2 * node + 2)
-
     def build(node: Int, start: Int, finish: Int): Unit = (start == finish) match {
-        case true  => t(node) = nums(start)
+        case true  => { t(node) = nums(start) }
         case false => {
             val mid = start + ((finish - start) >> 1)
-            val (gauche, droit) = coupling(node)
-            build(gauche, start, mid)
-            build(droit, mid + 1, finish)
-            t(node) = t(gauche) + t(droit)
+            build(2*node + 1, start, mid)
+            build(2*node + 2, mid + 1, finish)
+            t(node) = t(2*node + 1) + t(2*node + 2)
         }
     }
 
-    def propagate(node: Int, start: Int, finish: Int): Unit = node match {
-        case node if (l(node) == 0) => ()
-        case node                   => (l(node) & 1) match {
-            case 0 => l(node) = 0
-            case _ => {
-                t(node) = (finish - start + 1) - t(node)
-                if (start != finish) {
-                    val (left, right) = coupling(node)
-                    l(left)  = l(left)  ^ 1
-                    l(right) = l(right) ^ 1
-                }
-                l(node) = 0
+    def succession(node: Int, start: Int, finish: Int): Unit = (l(node) & 1) match {
+        case 0 => l(node) = 0
+        case _ => {
+            t(node) = (finish - start + 1) - t(node)
+            if (start != finish) {
+                l(2*node + 1) ^= 1
+                l(2*node + 2) ^= 1
             }
+            l(node) = 0
         }
     }
 
     def rangeUpdate(left: Int, right: Int): Unit = rangeUpdate(0, 0, n - 1, left, right)
 
-    def rangeUpdate(node: Int, start: Int, finish: Int, left: Int, right: Int): Unit = {
-        propagate(node, start, finish)
-        (start, finish) match {
-            case (start, finish) if (left > finish || right < start || start > finish) => ()
-            case (start, finish) if (left <= start && finish <= right) => {
-                t(node) = (finish - start + 1) - t(node)
-                if (start != finish) {
-                    val (gauche, droit) = coupling(node)
-                    l(gauche) = l(gauche) ^ 1
-                    l(droit)  = l(droit)  ^ 1
-                }
+    def rangeUpdate(node: Int, start: Int, finish: Int, left: Int, right: Int): Unit = (start, finish) match {
+        case (start, finish) if (left > finish || right < start)   => { succession(node, start, finish); () }
+        case (start, finish) if (left <= start && finish <= right) => {
+            succession(node, start, finish)
+            t(node) = (finish - start + 1) - t(node)
+            if (start != finish) {
+                l(2*node + 1) ^= 1
+                l(2*node + 2) ^= 1
             }
-            case _ => {
-                val mid = start + ((finish - start) >> 1)
-                val (gauche, droit) = coupling(node)
-                rangeUpdate(gauche, start, mid, left, right)
-                rangeUpdate(droit, mid + 1, finish, left, right)
-                t(node) = t(gauche) + t(droit)
-            }
+        }
+        case _ => {
+            succession(node, start, finish)
+            val mid = start + ((finish - start) >> 1)
+            rangeUpdate(2*node + 1, start, mid, left, right)
+            rangeUpdate(2*node + 2, mid + 1, finish, left, right)
+            t(node) = t(2*node + 1) + t(2*node + 2)
         }
     }
 
+    def rangeEvaluate(): Int = rangeEvaluate(0, 0, n - 1, 0, n - 1)
 
-    def countOfOnes(): Int = countOfOnes(0, 0, n - 1, 0, n - 1)
-
-    def countOfOnes(node: Int, start: Int, finish: Int, left: Int, right: Int): Int = (start, finish) match {
-        case (start, finish) if (left > finish || right < start || start > finish) => 0
-        case (start, finish) if (left <= start && finish <= right)                 => t(node)
-        case                                                                    _  => {
+    def rangeEvaluate(node: Int, start: Int, finish: Int, left: Int, right: Int): Int = (start, finish) match {
+        case (start, finish) if (left > finish || right < start)    => 0
+        case (start, finish) if (left <= start && finish <= right)  => t(node)
+        case                                                     _  => {
             val mid = start + ((finish - start) >> 1)
-            val (gauche, droit) = coupling(node)
-            countOfOnes(gauche, start, mid, left, right) + countOfOnes(droit, mid + 1, finish, left, right)
+            rangeEvaluate(2*node + 1, start, mid, left, right) + rangeEvaluate(2*node + 2, mid + 1, finish, left, right)
         }
     }
 }
@@ -83,7 +70,7 @@ object SegmentTree {
             val Array(queryType, left, right) = query
             queryType match {
                 case 1 => sg.rangeUpdate(left, right); (res, curr)
-                case 2 => (res, curr + left.toLong * sg.countOfOnes())
+                case 2 => (res, curr + left.toLong * sg.rangeEvaluate())
                 case 3 => (curr :: res, curr)
             }
         }.head.reverse.toArray
@@ -91,6 +78,5 @@ object SegmentTree {
 }
 
 object Solution {
-    def handleQuery(nums1: Array[Int], nums2: Array[Int], queries: Array[Array[Int]]): Array[Long] =
-    SegmentTree(nums1, nums2, queries)
+    def handleQuery(nums1: Array[Int], nums2: Array[Int], queries: Array[Array[Int]]): Array[Long] = SegmentTree(nums1, nums2, queries)
 }
