7e3e2debb64bc5da3d82ef55f2a336b076dc0902
varushvarsha <varush.varsha777@gmail.com>
2024-10-16 19:24:41 +0530

    added

---
+++ Leetcode/Erlang/1863. Sum of All Subset XOR Totals/sol/sol.erl
@@ -0,0 +1,20 @@
+-compile(export_all). % testing phase directive
+% "Ooarai Joshigakuin Shōri!"
+
+-spec subset_xor_sum(Nums :: [integer()]) -> integer().
+
+reduce([], Res) -> Res;
+reduce([First|Rest], Res) -> reduce(Rest, First bxor Res).
+
+rx(   _, Upper, Upper, Res) -> Res;
+rx(Nums, Upper, Index, Res) ->
+    Indices = lists:filter(fun(J) -> ((1 bsl J) band Index) == 0 end, lists:seq(0, length(Nums))),
+    Values  = lists:map(fun(I) -> case Nums == [] of
+                                     true  -> 0;
+                                     false -> lists:nth(I + 1, Nums)
+                                 end
+                       end , lists:droplast(Indices)),
+    rx(Nums, Upper, Index + 1, Res + reduce(Values, 0)).
+
+subset_xor_sum(Nums) ->
+    rx(Nums, 1 bsl length(Nums), 0, 0).
