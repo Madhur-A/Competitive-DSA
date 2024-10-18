0fc57c37309f990115148a364d325c7b2b336924
varushvarsha <varush.varsha777@gmail.com>
2024-10-18 18:22:56 +0530

    added

--- Leetcode/Scala/1847. Closest Room/Solution/src/main/scala/Solution.scala
+++ Leetcode/Scala/1847. Closest Room/Solution/src/main/scala/Solution.scala
@@ -2,13 +2,63 @@
 
 
 
-object Solution {
-    def closestRoom(rooms: Array[Array[Int]], queries: Array[Array[Int]]): Array[Int] = {
-    }
+// highly non functional
+// apparently this problem is higly repellant to
+// methods like .filter .map etc
 
-    def main(args: Array[String]) = {
-        val   rooms = Array(Array(2,2),Array(1,2),Array(3,2))
-        val queries = Array(Array(3,1),Array(3,3),Array(5,2))
-        println(closestRoom(rooms, queries).mkString("[", ",", "]"))
+class Solve(rooms: Array[Array[Int]]) {
+    val n = rooms.length
+    val sortedRooms = rooms.sortBy(_(1))
+
+    def findClosest(target: Int): Int = {
+        def rx(left: Int, right: Int): Int = (left < right) match {
+            case  true => {
+                val mid = left + ((right - left) >> 1)
+                sortedRooms(mid)(1) match {
+                    case x if (x < target) => rx(mid + 1, right)
+                    case x => rx(left, mid)
+                }
+            }
+            case false => left
+        }
+        rx(0, n)
     }
+}
+
+object Solve {
+    def apply(rooms: Array[Array[Int]], queries: Array[Array[Int]]): Array[Int] = {
+        val sol = new Solve(rooms)
+        val res = Array.fill(queries.length)(-1)
+        val sortedQeuries =
+
+        queries.zipWithIndex.sortBy(_._1(1)).foreach((query, qIndex) => {
+            val Array(preferred, minSize) = query
+            val closestRoomIndex = sol.findClosest(minSize)
+            val availableRooms = scala.collection.mutable.Set[Int]()
+            var minimumDifference = Int.MaxValue
+            var minimumPossibleRoom = Int.MaxValue
+            (closestRoomIndex until sol.n).foreach(e => {
+                val diff = math.abs(preferred - sol.sortedRooms(e)(0))
+                if (diff <= minimumDifference) {
+                    if (diff == minimumDifference) {
+                        minimumPossibleRoom = math.min(minimumPossibleRoom, sol.sortedRooms(e)(0))
+                    } else {
+                        minimumPossibleRoom = sol.sortedRooms(e)(0)
+                        minimumDifference = diff
+                    }
+                }
+            })
+
+            if (minimumPossibleRoom != Int.MaxValue) {
+                res(qIndex) = minimumPossibleRoom
+            }
+
+            // var ids = sortedRooms.filter(_(1) >= minSize).map(_(0)).toArray // this gives MLE
+        })
+        res
+    }
+}
+
+object Solution {
+    def closestRoom(rooms: Array[Array[Int]], queries: Array[Array[Int]]): Array[Int] = Solve(rooms, queries)
 }
