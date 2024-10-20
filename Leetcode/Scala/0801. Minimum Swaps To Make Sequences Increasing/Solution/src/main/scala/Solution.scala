bbfe31bfddfa6707b9a0fdf6b46add1273f0b8d7
varushvarsha <varush.varsha777@gmail.com>
2024-10-20 19:46:55 +0530

    added

--- Leetcode/Scala/0801. Minimum Swaps To Make Sequences Increasing/Solution/src/main/scala/Solution.scala
+++ Leetcode/Scala/0801. Minimum Swaps To Make Sequences Increasing/Solution/src/main/scala/Solution.scala
@@ -2,12 +2,13 @@
 
 
 
+// definitely not functional
+
 object Solution {
     def minSwap(nums1: Array[Int], nums2: Array[Int], reverse: Boolean = false): Int = reverse match {
         case false => minSwap(nums1.reverse, nums2.reverse, true)
         case  true => {
-            val (a, b, n) = (nums1.head, nums2.head, nums1.length)
-            val dp = Array.fill(n, 2)(-1)
+            val (n, dp) = (nums1.length, Array.fill(nums1.length, 2)(-1))
             def rx(a: Int = Int.MaxValue, b: Int = Int.MaxValue, index: Int = 0, swapped: Int = 0): Int = (index < n) match {
                 case false => 0
                 case  true => {
@@ -30,11 +31,5 @@ object Solution {
             }
             rx()
         }
-    }
-
-    def main(args: Array[String]): Unit = {
-        val (nums1, nums2) = (Array(1,3,5,4), Array(1,2,3,7)) //(Array(0,4,4,5,9), Array(0,1,6,8,10))
-        val res = minSwap(nums1, nums2)
-        println(s"result is $res")
     }
 }
