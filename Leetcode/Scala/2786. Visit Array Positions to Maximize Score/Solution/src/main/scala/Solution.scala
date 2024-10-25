3196b698df32e98705e269b7c35ee6c87d992f49
varushvarsha <varush.varsha777@gmail.com>
2024-10-26 00:24:42 +0530

    added

---
+++ Leetcode/Scala/2786. Visit Array Positions to Maximize Score/Solution/src/main/scala/Solution.scala
@@ -0,0 +1,16 @@
+
+
+
+
+object Solution {
+    val inf: Long = Int.MinValue
+
+    def maxScore(nums: Array[Int], k: Int): Long = {
+        val res = if ((nums.head & 1) == 1) Array(inf, nums.head.toLong) else Array(nums.head.toLong, inf)
+        nums.tail.foldLeft(res) { case (res, num) =>
+            val (i, j) = ((num & 1), (num & 1) ^ 1)
+            val (a, b) = (math.max(res(i) + num, res(j) + num - k), math.max(res(j), res(i) + num - k))
+            res(i) = a; res(j) = b; res
+        }.max
+    }
+}
