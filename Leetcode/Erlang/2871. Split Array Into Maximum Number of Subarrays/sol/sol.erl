2853279bd54f424feeb690735d269177a6fc43fd
varushvarsha <varush.varsha777@gmail.com>
2024-10-25 05:39:18 +0530

    added

---
+++ Leetcode/Erlang/2871. Split Array Into Maximum Number of Subarrays/sol/sol.erl
@@ -0,0 +1,15 @@
+-module(sol).
+-compile(export_all). % testing phase directive
+% "Ooarai Joshigakuin Shōri!"
+
+-spec max_subarrays(Nums :: [integer()]) -> integer().
+
+rx([],                _,     1) -> 1;
+rx([],                _, Count) -> Count - 1;
+rx([First | Rest], Curr, Count) ->
+    case (Curr band First) of
+        0 -> rx(Rest, -1, Count + 1);
+        _ -> rx(Rest, First band Curr, Count)
+    end.
+
+max_subarrays(Nums) -> rx(Nums, -1, 1).
