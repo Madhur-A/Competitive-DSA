7d59a9d0e43edc14a744e754d8b006926384fc47
varushvarsha <varush.varsha777@gmail.com>
2024-10-27 12:58:16 +0530

    added

--- Leetcode/CPP/3290__Maximum_Multiplication_Score/main.cpp
+++ Leetcode/CPP/3290__Maximum_Multiplication_Score/main.cpp
@@ -10,31 +10,31 @@ class Solution {
 public:
     inline static constexpr long const inf = LONG_MIN / 2;
 public:
-	std::vector<std::vector<long>> dp;
+    std::vector<std::vector<long>> dp;
 public:
-	// top down
-	long rx(std::vector<int> const &a, std::vector<int> const &b, int count, int index) {
-		int const n = b.size();
+    // top down
+    long rx(std::vector<int> const &a, std::vector<int> const &b, int count, int index) {
+        int const n = b.size();
         if(count == 4) { return 0;   }
-		if(n <= index) { return inf; }
+        if(n <= index) { return inf; }
         if(count >  4) { return inf; }
 
-		if(dp[count][index] != inf) { return dp[count][index]; }
+        if(dp[count][index] != inf) { return dp[count][index]; }
 
         long const skip = rx(a, b, count, index + 1);
         long const pick = rx(a, b, count + 1, index + 1) + a[count] * 1L * b[index];
 
-		return dp[count][index] = std::max(pick, skip);
-	}
+        return dp[count][index] = std::max(pick, skip);
+    }
 
     long long maxScore(vector<int>& a, vector<int>& b) {
-		int const n = b.size();
-		dp.resize(5, std::vector<long>(n + 1, inf));
+        int const n = b.size();
+        dp.resize(5, std::vector<long>(n + 1, inf));
 
-		// return rx(a, b, 0, 0); // top -down
+        // return rx(a, b, 0, 0); // top -down
 
-		/*** Bottom Up ***/
-		for(int i = 0; i < n + 1; ++i) { dp[0][i] = 0; }
+        /*** Bottom Up ***/
+        for(int i = 0; i < n + 1; ++i) { dp[0][i] = 0; }
         for(int j = 0; j < 5; ++j) { dp[j][j] = 0;}
 
         for(int i = 1; i < 5; ++i) {
@@ -48,10 +48,10 @@ public:
 };
 
 int main() {
-	Solution sol;
-	std::vector<int> a =  {3,2,5,6}, b = {2,-6,4,-5,-3,2,-7};
+    Solution sol;
+    std::vector<int> a =  {3,2,5,6}, b = {2,-6,4,-5,-3,2,-7};
 
 
 
-	return std::fprintf(stdout, "%lld\n", sol.maxScore(a, b));
+    return std::fprintf(stdout, "%lld\n", sol.maxScore(a, b));
 }
