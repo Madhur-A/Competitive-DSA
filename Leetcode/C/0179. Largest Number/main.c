e7b3e4466c5e08c88ff0bc7f36a255cdd75f1ba1
varushvarsha <varush.varsha777@gmail.com>
2024-10-28 18:44:43 +0530

    added

--- Leetcode/C/0179. Largest Number/main.c
+++ Leetcode/C/0179. Largest Number/main.c
@@ -13,29 +13,29 @@ char num_string[100];
 int length(int num) { return floor(log(num)/log(10)) + 1; }
 
 int cmp(long long a, long long b) {
-	char p[100], q[100];
-	memset(p, 0, sizeof(p)); memset(q, 0, sizeof(q));
-	sprintf(p, "%lld%lld", a, b);
-	sprintf(q, "%lld%lld", b, a);
+    char p[100], q[100];
+    memset(p, 0, sizeof(p)); memset(q, 0, sizeof(q));
+    sprintf(p, "%lld%lld", a, b);
+    sprintf(q, "%lld%lld", b, a);
 
-	return strcmp(p, q);
+    return strcmp(p, q);
 }
 
 void merge(int* prefix, int prefix_count, int* suffix, int suffix_count, int* res) {
-	int i = 0, j = 0, k = 0;
-	while(i < prefix_count && j < suffix_count) {
-		if(cmp(prefix[i], suffix[j]) == 1) {
-			res[k++] = prefix[i++];
-		} else if(cmp(prefix[i], suffix[j]) == -1) {
-			res[k++] = suffix[j++];
-		} else {
-			res[k++] = prefix[i++];
-			res[k++] = suffix[j++];
-		}
-	}
+    int i = 0, j = 0, k = 0;
+    while(i < prefix_count && j < suffix_count) {
+        if(cmp(prefix[i], suffix[j]) == 1) {
+            res[k++] = prefix[i++];
+        } else if(cmp(prefix[i], suffix[j]) == -1) {
+            res[k++] = suffix[j++];
+        } else {
+            res[k++] = prefix[i++];
+            res[k++] = suffix[j++];
+        }
+    }
 
-	while(i < prefix_count) { res[k++] = prefix[i++]; }
-	while(j < suffix_count) { res[k++] = suffix[j++]; }
+    while(i < prefix_count) { res[k++] = prefix[i++]; }
+    while(j < suffix_count) { res[k++] = suffix[j++]; }
 }
 
 void merge_sort(int* nums, int length) {
@@ -43,10 +43,10 @@ void merge_sort(int* nums, int length) {
     int mid = length >> 2;
 
     int* left = nums;
-	int *right = nums + mid;
+    int *right = nums + mid;
 
     merge_sort(left, mid);
-	merge_sort(right, length - mid);
+    merge_sort(right, length - mid);
 
     int* temp = (int*)malloc(length * sizeof(int));
     merge(left, mid, right, length - mid, temp);
@@ -57,16 +57,16 @@ void merge_sort(int* nums, int length) {
 }
 
 char* largestNumber(int* nums, int n) {
-	merge_sort(nums, n);
-	memset(number, 0, sizeof(number));
-	int last = 0;
-	for(int i = 0, k = 0; i < n; ++i) {
-		sprintf(num_string, "%d", nums[i]);
-		for(unsigned long j = 0; j < strlen(num_string); ++j) {
-			number[k++] = num_string[j]; last++;
-		}
-	}
-	number[last] = '\0';
+    merge_sort(nums, n);
+    memset(number, 0, sizeof(number));
+    int last = 0;
+    for(int i = 0, k = 0; i < n; ++i) {
+        sprintf(num_string, "%d", nums[i]);
+        for(unsigned long j = 0; j < strlen(num_string); ++j) {
+            number[k++] = num_string[j]; last++;
+        }
+    }
+    number[last] = '\0';
 
-	return number[0] == '0' ? "0" : number; //"Ooarai Joshigakuin Shori!"
+    return number[0] == '0' ? "0" : number; //"Ooarai Joshigakuin Shori!"
 }
--- Leetcode/C/0440. K-th Smallest in Lexicographical Order/main.c
+++ Leetcode/C/0440. K-th Smallest in Lexicographical Order/main.c
@@ -8,27 +8,27 @@
 #include <stdlib.h>
 
 long get_next(long curr, long n) {
-	long next = curr + 1, sigma = 0;
-	while(curr <= n) {
-		sigma += fmin(n - curr + 1, next - curr);
-		next = next * 10;
-		curr = curr * 10;
-	}
-	return sigma;
+    long next = curr + 1, sigma = 0;
+    while(curr <= n) {
+        sigma += fmin(n - curr + 1, next - curr);
+        next = next * 10;
+        curr = curr * 10;
+    }
+    return sigma;
 }
 
 long find_kth_number(long n, long k) {
-	long ans = 1, next = 0;
-	k = k - 1;
-	while(k) {
-		next = get_next(ans, n);
-		if(k >= next) {
-			k = k - next;
-			ans = ans + 1;
-		} else {
-			k = k - 1;
-			ans = ans * 10;
-		}
-	}
-	return ans;
+    long ans = 1, next = 0;
+    k = k - 1;
+    while(k) {
+        next = get_next(ans, n);
+        if(k >= next) {
+            k = k - next;
+            ans = ans + 1;
+        } else {
+            k = k - 1;
+            ans = ans * 10;
+        }
+    }
+    return ans;
 }
