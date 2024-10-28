3b23011ab2c1d4fc58d4cd9d11a434209484d388
varushvarsha <varush.varsha777@gmail.com>
2024-10-28 21:49:28 +0530

    added

--- Leetcode/Scala/0539. Minimum Time Difference/Solution.scala
+++ Leetcode/Scala/0539. Minimum Time Difference/Solution.scala
@@ -7,18 +7,17 @@ object Solution {
         val Array(hours, minutes) = timeString.split(":").map(_.toInt)
         hours * 60 + minutes
     }
-    
-    def getRes(times: List[Int], res: Int = Int.MaxValue): Int = {
-        if (times.length == 2) { math.min(res, times.last - times.head) } else {
-            val newRes = times.tail.head - times.head
-            getRes(times.tail, math.min(res, newRes))
-        }
+
+    def getRes(times: List[Int], res: Int = Int.MaxValue): Int = (times.length == 2) match {
+        case  true => math.min(res, times.last - times.head)
+        case false => getRes(times.tail, math.min(res, times.tail.head - times.head))
     }
-    
-    def findMinDifference(timePoints: List[String]): Int = {
-        if (timePoints.length <= 1440) {
+
+    def findMinDifference(timePoints: List[String]): Int = (timePoints.length <= 1440) match {
+        case  true => {
             val listOfTimes = timePoints.map(parse).sorted
             getRes(listOfTimes.appended(listOfTimes.head + 1440))
-        } else { 0 }
+        }
+        case false => 0
     }
 }
