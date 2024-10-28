68b3b8accd1d3e2be22b09aa578b502f79dfc2cc
varushvarsha <varush.varsha777@gmail.com>
2024-10-28 20:46:51 +0530

    added

--- Leetcode/Scala/0300. Longest Increasing Subsequence/src/main/scala/Solution.scala
+++ Leetcode/Scala/0300. Longest Increasing Subsequence/src/main/scala/Solution.scala
@@ -43,8 +43,9 @@ object Solution {
         nums.indices.drop(1).foldLeft(Array.fill(nums.length)(1)) { case (dp, index) =>
             dp.update(index, Math.max(dp(index), {
                 dp.take(index).indices.foldLeft(0) { case (innerMax, j) =>
-                    if (nums(index) <= nums(j)) innerMax else {
-                        Math.max(innerMax, dp(j) + 1)
+                    (nums(index) <= nums(j)) match {
+                        case  true => innerMax
+                        case false => math.max(innerMax, dp(j) + 1)
                     }
                 }
             })); dp
