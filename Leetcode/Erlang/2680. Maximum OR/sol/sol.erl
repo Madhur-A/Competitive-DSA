f9c1ab1d83b83d06f03d87667f5b2d8044aa2ff7
varushvarsha <varush.varsha777@gmail.com>
2024-10-29 11:58:05 +0530

    added

--- Leetcode/Erlang/2680. Maximum OR/sol/sol.erl
+++ Leetcode/Erlang/2680. Maximum OR/sol/sol.erl
@@ -8,11 +8,10 @@ maximum_or(Nums, K) ->
     {PList, _} = lists:mapfoldl(fun(E, Res) -> {Res, E bor Res} end, 0, Nums),
     {SList, _} = lists:mapfoldr(fun(E, Res) -> {Res, E bor Res} end, 0, Nums),
     {P,     S} = {array:from_list(PList), array:from_list(SList)},
-    {Res,  _ } = lists:foldl(
-                   fun(Num, {Res, Index}) ->
-                           {
-                             max(Res, array:get(Index, P) bor (Num bsl K) bor array:get(Index, S)),
-                             Index + 1
-                           }
-                   end, {0, 0}, Nums),
-    Res.
+    element(1,
+            lists:foldl(fun(Num, {Res, Index}) ->
+                                {
+                                  max(array:get(Index, P) bor (Num bsl K) bor array:get(Index, S), Res),
+                                  Index + 1
+                                }
+                        end, {0, 0}, Nums)).
