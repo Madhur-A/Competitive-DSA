3c38a5a51f39ed38a3719de7c350cbd00e5059dc
varushvarsha <varush.varsha777@gmail.com>
2024-11-01 09:02:23 +0530

    added

--- Leetcode/Scala/1957. Delete Characters to Make Fancy String/Solution/src/main/scala/Solution.scala
+++ Leetcode/Scala/1957. Delete Characters to Make Fancy String/Solution/src/main/scala/Solution.scala
@@ -4,12 +4,12 @@
 
 object Solution {
     def makeFancyString(s: String): String = {
-        s.foldLeft((List[Char](), 1, '1')) { case ((res, count, prev), curr) =>
+        s.foldRight((List[Char](), 1, '1')) { case (curr, (res, count, prev)) =>
             val newCount = if (curr == prev) count + 1 else 1
             newCount match {
                 case x if (3 <= x) => (res, newCount, curr)
                 case            _  => (curr :: res, newCount, curr)
             }
-        }.head.reverse.mkString
+        }.head.mkString
     }
 }
