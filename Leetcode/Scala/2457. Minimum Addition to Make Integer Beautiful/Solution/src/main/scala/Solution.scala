00f5c427c24bcb8402cf59ef3df76b69982c913f
varushvarsha <varush.varsha777@gmail.com>
2024-11-03 02:24:57 +0530

    added

--- Leetcode/Scala/2457. Minimum Addition to Make Integer Beautiful/Solution/src/main/scala/Solution.scala
+++ Leetcode/Scala/2457. Minimum Addition to Make Integer Beautiful/Solution/src/main/scala/Solution.scala
@@ -5,9 +5,8 @@
 object Solution {
     def makeIntegerBeautiful(n: Long, k: Int): Long = {
         def rx(num: Long, mux: Long = 1L): Long = {
-            val currS = num.toString
-            val currV = currS.map(_.asDigit).sum.toLong
-            currV <= k match {
+            val curr = num.toString.map(_.asDigit).sum.toLong
+            curr <= k match {
                 case  true => 0
                 case false => mux * (10L - num % 10L) + rx(1L + (num / 10L), mux * 10L)
             }
