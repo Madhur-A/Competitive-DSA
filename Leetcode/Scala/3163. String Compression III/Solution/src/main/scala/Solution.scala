8141607f177b388bc0359edf1976abe3380d4425
varushvarsha <varush.varsha777@gmail.com>
2024-11-05 09:42:25 +0530

    added

---
+++ Leetcode/Scala/3163. String Compression III/Solution/src/main/scala/Solution.scala
@@ -0,0 +1,32 @@
+
+
+
+
+object Solution {
+    def compressedString(word: String): String = {
+
+        val repeatThis: (Int, Char) => String = (n, c) => s"${c}9" * n
+
+        def calculate(n: Int, c: Char): String = (n <= 9) match {
+            case false => n % 9 match {
+                case 0 => repeatThis(n / 9, c)
+                case _ => s"${c}${n % 9}${repeatThis(n / 9, c)}"
+            }
+            case  true => s"${c}${n}"
+        }
+
+        def rx(inputString: String, prev: Char, res: String = ""): String = inputString.isEmpty match {
+            case  true => res
+            case false => {
+                val count = inputString.takeWhile(_ == prev).length
+                val newString = inputString.dropWhile(_ == prev)
+                newString.isEmpty match {
+                    case  true => calculate(count, prev) + res
+                    case false => rx(newString, newString.head, calculate(count, prev) + res)
+                }
+            }
+        }
+
+        rx(word, word.head).reverse
+    }
+}
