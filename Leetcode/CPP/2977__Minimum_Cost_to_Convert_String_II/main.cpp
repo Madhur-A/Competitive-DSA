62bac39be0e04d6937f3d3734a1209d01b7ebec5
varushvarsha <varush.varsha777@gmail.com>
2026-01-30 22:45:13 +0530

    fixed

--- Leetcode/CPP/2977__Minimum_Cost_to_Convert_String_II/main.cpp
+++ Leetcode/CPP/2977__Minimum_Cost_to_Convert_String_II/main.cpp
@@ -2,102 +2,124 @@
 
 
 
-#include <bits/stdc++.h>
+#include <array>
+#include <vector>
+#include <string>
+#include <cstdio>
+#include <climits>
 
 using std::vector;
 using std::string;
 
-class Solution {
+struct trie_node {
 public:
-    inline static constexpr int const inf = INT_MAX/2;
+    std::array<trie_node*, 26> children;
+    int node_idx = -1;
 public:
-    using element = std::pair<int, std::string>; // cost, index
-public:
-    std::unordered_map<std::string, std::vector<element>> g;
-    std::unordered_map<std::string, std::vector<int>> source_index;
-    std::unordered_map<std::string, std::vector<int>> target_index;
-    std::priority_queue<element, std::vector<element>, std::greater<element>> t;
-public:
-    std::vector<std::vector<int>> dp;
-public:
-    void dijkstra(int index, vector<string>& original, vector<string>& changed) {
-        t.push({0, original[index]});
-        while(not t.empty()) {
-            element const popped = t.top(); t.pop();
-            int const curr = popped.first;
-            std::string const node = popped.second;
-            for(element const &ch_node: g[node]) {
-                int const ch_cost = ch_node.first;
-                std::string const ch_string = ch_node.second;
-                for(int const &child: target_index[ch_string]) {
-                    if(ch_cost + curr < dp[index][child]) {
-                        dp[index][child] = ch_cost + curr;
-                        t.push({dp[index][child], ch_string});
-                    }
-                }
-            }
-        }
-    }
-
-    long dfs(string &source, string target, int index) {
-        int const n = source.length();
-        while(index < n and source[index] == target[index]) { ++index; }
-        if(index >= n or source == target) { return 0L; }
-        long res = inf;
-        for(int sz = n-1; sz >= 1; --sz) {
-            std::string const s_string = source.substr(index, sz), t_string = target.substr(index, sz);
-            if(source_index.count(s_string) and target_index.count(t_string)) {
-                for(int const &s_index: source_index[s_string]) {
-                    for(int const &t_index: target_index[t_string]) {
-                        source = source.substr(0, index) + t_string + source.substr(index+sz);
-                        res = std::min(res, dp[s_index][t_index] + dfs(source, target, index+1));
-                    }
-                }
-            }
-        }
-        return res;
-    }
-
-    long long minimumCost(string source,
-                          string target, vector<string>& original,
-                                         vector<string>& changed, vector<int>& cost) {
-        int const n = original.size();
-        dp.resize(original.size(), std::vector<int>(changed.size(), inf));
-        for(int i = 0; i < n; ++i) {
-            g[original[i]].push_back({cost[i], changed[i]});
-            source_index[original[i]].push_back(i);
-            target_index[changed[i]].push_back(i);
-        }
-
-        for(int i = 0; i < n; ++i) {
-            dijkstra(i, original, changed);
-        }
-
-        for(int i = 0; i < n; ++i) {
-            for(int j = 0; j < changed.size(); ++j) {
-                if(original[i] == changed[j]) {
-                    dp[i][j] = 0;
-                }
-            }
-        }
-
-        long res = 0L;
-        int const sx = source.length();
-
-        for(int i = 0; i < sx-1; ++i) {
-            if(source[i] == target[i]) { continue; }
-            int j = i;
-            while(source[j] != target[j]) { ++j; }
-            std::string const s = source.substr(i, j-i+1), t = target.substr(i, j-i+1);
-            if(source_index.count(s) == 0 or target_index.count(t) == 0) { continue; }
-            int const si = source_index[s][0], rj = target_index[t][0];
-            res += dp[si][rj];            
-            source = source.substr(0, i) + t + source.substr(i + s.size());
-        }
-
-        return res;
+    inline trie_node() {
+        children.fill(nullptr);
     }
 };
+
+class Solution {
+public:
+    static constexpr long long inf = LLONG_MAX >> 1;
+public:
+    trie_node* root = new trie_node();
+    int next_idx = 0;
+public:
+    std::vector<std::vector<long long>> cost_graph;
+    std::vector<long long> seen;
+    std::string source_str;
+    std::string target_str;
+public:
+    inline int insert_into_trie(const std::string& word) {
+        trie_node* curr = root;
+        for (char const& c: word) {
+            int const idx = c - 'a';
+            if (curr->children[idx] == nullptr) {
+                curr->children[idx] = new trie_node();
+            }
+            curr = curr->children[idx];
+        }
+        if (curr->node_idx < 0) {
+            curr->node_idx = next_idx++; //!!!
+        }
+
+        return curr->node_idx;
+    }
+
+    inline long long dfs(int const& pos) {
+        int const n = source_str.length();
+        if (pos >= n) { return 0; }
+        if (seen[pos] != -1) { return seen[pos]; }
+
+        long long min_cost = inf;
+        if (source_str[pos] == target_str[pos]) {
+            min_cost = dfs(pos + 1);
+        }
+
+        trie_node* src_node = root;
+        trie_node* tgt_node = root;
+
+        for (int end = pos; end < n; ++end) {
+            src_node = src_node->children[source_str[end] - 'a'];
+            tgt_node = tgt_node->children[target_str[end] - 'a'];
+
+            if (src_node == nullptr or tgt_node == nullptr) { break; }
+            if (src_node->node_idx < 0 or tgt_node->node_idx < 0) { continue; }
+
+            long long transform_cost = cost_graph[src_node->node_idx][tgt_node->node_idx];
+            if (transform_cost < inf) {
+                long long candidate = transform_cost + dfs(end + 1);
+                if (candidate < min_cost) { min_cost = candidate; }
+            }
+        }
+
+
+        return seen[pos] = min_cost;
+    }
+
+public:
+    long long minimumCost(std::string source,
+                          std::string target,
+                          std::vector<std::string>& original,
+                          std::vector<std::string>& changed,
+                          std::vector<int>& cost) {
+        int const n = cost.size();
+        cost_graph.assign(n << 1, std::vector<long long>(n << 1, inf));
+        source_str = source;
+        target_str = target;
+
+        for (int i = 0; i < static_cast<int>(cost_graph.size()); ++i) {
+            cost_graph[i][i] = 0;
+        }
+
+        for (int i = 0; i < n; ++i) {
+            int const from_idx = insert_into_trie(original[i]);
+            int const to_idx   = insert_into_trie(changed[i]);
+            cost_graph[from_idx][to_idx] = std::min(cost_graph[from_idx][to_idx],
+                                                    static_cast<long long>(cost[i]));
+        }
+
+        for (int k = 0; k < next_idx; ++k) {
+            for (int i = 0; i < next_idx; ++i) {
+                if (cost_graph[i][k] >= inf) { continue; }
+                for (int j = 0; j < next_idx; ++j) {
+                    cost_graph[i][j] = std::min(cost_graph[i][j],
+                                                cost_graph[i][k] + cost_graph[k][j]);
+                }
+            }
+        }
+
+        seen.assign(source_str.length(), -1);
+        long long const res = dfs(0);
+
+
+        return res >= inf ? -1 : res;
+    }
+};
+
 
 int main() {
     Solution sol;
